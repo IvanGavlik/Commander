e507b0d4939685e58a2e048b4a58471307235d28
Ivan Gavlik <ivgavlik@vsite.hr>
2018-10-31 15:35:56 +0100

    model

---
+++ Commander/model/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commander.model
+{
+    enum Status
+    {
+        ON, EXECUTING_COMMAND, OFF
+    }
+}
